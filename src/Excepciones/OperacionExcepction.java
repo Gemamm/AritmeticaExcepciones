ec197f98807fa2546aa895532e5aa8c15f15a1c4
gemamm <gemamm1988@gmail.com>
2021-10-15 11:44:13 +0200

    act

--- src/Excepciones/OperacionExcepction.java
+++ src/Excepciones/OperacionExcepction.java
@@ -3,7 +3,7 @@ package Excepciones;
 
 
 public class OperacionExcepction extends Exception{
-    
+    /*siempre deben ser mandadas, tener un bloque try/catch*/
     public OperacionExcepction(String mensaje){
         super(mensaje);
     }
--- src/test/Test.java
+++ src/test/Test.java
@@ -12,6 +12,8 @@ public class Test {
             res = division(10,0);
         } catch (Exception e) {
             e.printStackTrace(System.out);
+        }finally {// excepcion de la estructura, se va ha ejecutar siempre
+            System.out.println("Siempre estoy!!");
         }
     }
     
