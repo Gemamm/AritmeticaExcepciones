aa92e058cdf3ada52c485cd88c5a1b3c18979e6e
gemamm <gemamm1988@gmail.com>
2021-10-15 11:46:52 +0200

    act

---
+++ src/Excepciones/Runtime.java
@@ -0,0 +1,13 @@
+
+
+package Excepciones;
+
+
+public class Runtime extends RuntimeException{
+    /*siempre deben ser mandadas, tener un bloque try/catch*/
+    public Runtime(String mensaje){
+        super(mensaje);
+    }
+    
+    
+}
